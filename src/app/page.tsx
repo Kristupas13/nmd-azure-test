b2fac257a4442f6894fcbd3018263f969a8bb9fd
Kristupas13 <51354779+Kristupas13@users.noreply.github.com>
2025-11-29 01:49:48 +0200

    Implement caching in Home component
    Added caching functionality to the Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { cacheLife } from "next/cache";
 import { mockDataWithDelay } from "./client";
 import RevalidateButton from "./revalidateButton";
 
 export default async function Home() {
+  "use cache";
+  cacheLife("days");
+  
   const data = await mockDataWithDelay();
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
